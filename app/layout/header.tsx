3a1ce76fe449c713e140eacb174586ee33bb83ec
ItsLok0 <pierre.parent2020@gmail.com>
2026-04-23 16:13:13 +0200

    suppression use client inutile + mise en avant header

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -3,7 +3,7 @@ import NavLinks from "@/app/ui/components/nav-links";
 
 export default function Header() {
     return (
-        <header className="sticky top-0 bg-bg-page border-b-2 border-primary">
+        <header className="sticky z-1 top-0 bg-bg-page border-b-2 border-primary">
             <NavLinks />
         </header>
     );
